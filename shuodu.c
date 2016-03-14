850c77477c659cbf61a65de671067a0e1812edd6
ozhu <ollie.zhu@gmail.com>
2016-03-14 00:46:03 -0700

    disable curses for now, handle options

--- shuodu.c
+++ shuodu.c
@@ -3,22 +3,27 @@
  * with ncurses
  * http://invisible-island.net/ncurses/ncurses-intro.html
  * http://www.writeka.com/ed/ncurses_library.html
- * tint
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <errno.h>
+#include <string.h>
 #include <curses.h>
 #include <signal.h>
 
 #define CELL_WIDTH 3
-#define BOX_WIDTH (3*CELL_WIDTH + 2) // 11
-#define GRID_WIDTH (3*BOX_WIDTH + 2) // 35
+#define BOX_WIDTH (3*CELL_WIDTH + 2)    /* 11 */
+#define GRID_WIDTH (3*BOX_WIDTH + 2)    /* 35 */
 
 static void finish(int sig);
 static void draw_grid(void);
 void set_cell(int, int);
 int handle_ch(int);
 int gy = 1, gx = 1;
+
+long int difficulty = -1;
+#define MAX_DIFFICULTY  4
 
 /*
  * Grid things
@@ -82,66 +87,6 @@ handle_ch(int c)
     }
 }
 
-int
-main(int argc, char *argv[])
-{
-    int num = 0;
-
-    /* initialize your non-curses data structures here */
-    //printgrid();
-
-#if 1
-    (void) signal(SIGINT, finish);      /* arrange interrupts to terminate */
-
-    initscr();      /* initialize the curses library */
-    draw_grid();
-    //(void) newterm();
-    keypad(stdscr, TRUE);  /* enable keyboard mapping */
-    (void) nonl();         /* tell curses not to do NL->CR/NL on output */
-    (void) cbreak();       /* take input chars one at a time, no wait for \n */
-#if 0
-    (void) echo();         /* echo input - in color */
-#else
-    (void) noecho();
-#endif
-
-    if (has_colors())
-    {
-        start_color();
-
-        /*
-         * Simple color assignment, often all we need.  Color pair 0 cannot
-         * be redefined.  This example uses the same value for the color
-         * pair as for the foreground color, though of course that is not
-         * necessary:
-         */
-        init_pair(1, COLOR_RED,     COLOR_BLACK);
-        init_pair(2, COLOR_GREEN,   COLOR_BLACK);
-        init_pair(3, COLOR_YELLOW,  COLOR_BLACK);
-        init_pair(4, COLOR_BLUE,    COLOR_BLACK);
-        init_pair(5, COLOR_CYAN,    COLOR_BLACK);
-        init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
-        init_pair(7, COLOR_WHITE,   COLOR_BLACK);
-    }
-    init_pair(8, COLOR_WHITE, COLOR_BLACK);
-    bkgd(COLOR_PAIR(8));//wbkgd();
-
-    set_cell(gy, gx);
-
-    for (;;)
-    {
-        int c = getch();     /* refresh, accept single keystroke of input */
-        handle_ch(c);
-        attrset(COLOR_PAIR(num % 8));
-        num++;
-
-        /* process the command keystroke */
-    }
-
-    finish(0);               /* we're done */
-#endif
-}
-
 static void
 draw_grid(void)
 {
@@ -194,5 +139,131 @@ static void finish(int sig)
     /* do your non-curses wrapup here */
 
     exit(0);
+}
+
+void
+print_usage(FILE *stream)
+{
+    fprintf(stream, "Usage: shuodu [-h | --help] [-d N]\n");
+    fprintf(stream, "  -h, --help    - Print this help message and exit.\n");
+    fprintf(stream, "  -d <N>,       - Specify the difficulty N (1-4).\n");
+}
+
+int
+parse_difficulty(char *str)
+{
+    char *endptr;
+
+    errno = 0;
+    difficulty = strtol(str, &endptr, 10);
+
+    if ((errno == ERANGE && (difficulty == LONG_MAX || difficulty == LONG_MIN)) || (errno != 0 && difficulty == 0)) {
+        perror("strtol");
+        return 1;
+    }
+    if (endptr == str) {
+        fprintf(stderr, "No digits were found.\n");
+        return 1;
+    }
+    if (*endptr != '\0') {
+        fprintf(stderr, "Invalid argument %s.\n", endptr);
+        return 1;
+    }
+    if (difficulty < 0 || difficulty > MAX_DIFFICULTY) {
+        fprintf(stderr, "Difficulty level %ld out of range.\n", difficulty);
+        return 1;
+    }
+
+    return 0;
+}
+
+static void
+parse_options(int argc, char *argv[])
+{
+    char *option;
+
+    for (int i = 1; i < argc; i++) {
+        option = argv[i];
+        if (!strcmp(option, "-h") || !strcmp(option, "--help")) {
+            fprintf(stdout, "shuodu, a sudoku player in the terminal.\n\n");
+            print_usage(stdout);
+            exit(EXIT_SUCCESS);
+        } else if (!strcmp(option, "-d")) {
+            i++;
+            if (i >= argc || parse_difficulty(argv[i])) {
+                print_usage(stderr);
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            fprintf(stderr, "%s: Invalid option `%s'\n", argv[0], argv[i]);
+            print_usage(stderr);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+//#define CURSES
+int
+main(int argc, char *argv[])
+{
+    parse_options(argc, argv);
+    //printf("shuodu hello world argc %d argv %p\n", argc, argv);
+
+    return 0;
+
+#ifdef CURSES
+    int num = 0;
+
+    /* initialize your non-curses data structures here */
+    //printgrid();
+
+    (void) signal(SIGINT, finish);      /* arrange interrupts to terminate */
+
+    initscr();      /* initialize the curses library */
+    draw_grid();
+    //(void) newterm();
+    keypad(stdscr, TRUE);  /* enable keyboard mapping */
+    (void) nonl();         /* tell curses not to do NL->CR/NL on output */
+    (void) cbreak();       /* take input chars one at a time, no wait for \n */
+#if 0
+    (void) echo();         /* echo input - in color */
+#else
+    (void) noecho();
+#endif
+
+    if (has_colors()) {
+        start_color();
+
+        /*
+         * Simple color assignment, often all we need.  Color pair 0 cannot
+         * be redefined.  This example uses the same value for the color
+         * pair as for the foreground color, though of course that is not
+         * necessary:
+         */
+        init_pair(1, COLOR_RED,     COLOR_BLACK);
+        init_pair(2, COLOR_GREEN,   COLOR_BLACK);
+        init_pair(3, COLOR_YELLOW,  COLOR_BLACK);
+        init_pair(4, COLOR_BLUE,    COLOR_BLACK);
+        init_pair(5, COLOR_CYAN,    COLOR_BLACK);
+        init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
+        init_pair(7, COLOR_WHITE,   COLOR_BLACK);
+    }
+    init_pair(8, COLOR_WHITE, COLOR_BLACK);
+    bkgd(COLOR_PAIR(8));//wbkgd();
+
+    set_cell(gy, gx);
+
+    for (;;)
+    {
+        int c = getch();     /* refresh, accept single keystroke of input */
+        handle_ch(c);
+        attrset(COLOR_PAIR(num % 8));
+        num++;
+
+        /* process the command keystroke */
+    }
+
+    finish(0);               /* we're done */
+#endif /* CURSES */
 }
 
